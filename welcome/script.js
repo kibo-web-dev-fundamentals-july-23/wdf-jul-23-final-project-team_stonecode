29b295a3eb3705b2213dccbce82d3304cdf45254
Abdulkareem Sikirulahi <sikirulahi.abdulkareem@kibo.school>
2023-09-08 18:00:40 +0100

    sikirulahi commit

---
+++ welcome/script.js
@@ -0,0 +1,5 @@
+// Add an event listener to the button to navigate to the calculator page
+document.getElementById('enterButton').addEventListener('click', function () {
+    // Redirect to the calculator page (replace 'calculator.html' with your actual calculator page URL)
+    window.location.href = '../calculator/index.html';
+});
