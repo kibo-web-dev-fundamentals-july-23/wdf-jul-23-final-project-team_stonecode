fe7fa8ef9533a1d2cd61aa8a208ba1be0f56a7ce
Ayomide Dipeolu <ayomide.dipeolu@kibo.school>
2023-09-09 19:21:35 +0100

    finally done

--- script.js
+++
@@ -1,5 +0,0 @@
-// Add an event listener to the button to navigate to the calculator page
-document.getElementById('enterButton').addEventListener('click', function () {
-    // Redirect to the calculator page (replace 'calculator.html' with your actual calculator page URL)
-    window.location.href = '../calculator/index.html';
-});
